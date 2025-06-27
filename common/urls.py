70a67e6315c5fd5855ab92a9a676b93b712350bb
jamoliddin007 <jamoliddin_007>
2025-06-27 14:35:23 +0500

    Add DELETE endpoint for MediaFile: MediaFileDestroyAPIView

--- common/urls.py
+++ common/urls.py
@@ -1,7 +1,9 @@
 # common/urls.py
 
 from django.urls import path
-from common.api_endpoints import MediaFileCreateAPIView
+from common.api_endpoints import MediaFileCreateAPIView, MediaFileDestroyAPIView
 urlpatterns = [
     path('media/upload/', MediaFileCreateAPIView.as_view(), name='media-upload'),
+    path("media/delete/<int:id>/", MediaFileDestroyAPIView.as_view(), name="media-delete"),
+
 ]
